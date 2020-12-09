de1bfd446500d7d8c3b97b17b24eb5492173f118
Kacper Malkowski <malkowski.kacperum@gmail.com>
2020-12-09 21:04:36 +0100

    add basic form styles

---
+++ src/views/Register.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-center items-center flex-grow">
+    <form class="form">
+      <div class="form-group">
+        <input id="username" class="form-input" required />
+        <label for="username" class="form-label">Username</label>
+      </div>
+      <div class="form-group">
+        <input id="firstname" class="form-input" required />
+        <label for="firstname" class="form-label">First name</label>
+      </div>
+    </form>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Register"
+});
+</script>
+<style lang="scss" scoped>
+@import "./src/assets/styles/form";
+</style>
